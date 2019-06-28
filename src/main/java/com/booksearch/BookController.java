aebbfbd284b9dcb33275700995e1a89f0b0f4b4a
Ed Werner <android.devops@gmail.com>
2019-06-28 16:43:41 -0400

    adds books reactive context variable

--- src/main/java/com/booksearch/BookController.java
+++ src/main/java/com/booksearch/BookController.java
@@ -1,6 +1,7 @@
 package com.booksearch;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.thymeleaf.spring5.context.webflux.IReactiveDataDriverContextVariable;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
+
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
@@ -15,19 +24,25 @@ import static org.springframework.web.reactive.function.server.ServerResponse.ok
 @Controller
 public class BookController {
 
-//    private final BookRepository bookRepository;
-//
-//    BookController(BookRepository bookRepository) {
-//        this.bookRepository = bookRepository;
-//    }
+    private final BookRepository bookRepository;
+
+    BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    
     @Bean
     RouterFunction<ServerResponse> routes(BookRepository br) {
         return RouterFunctions.route(GET("/bookfeed"), serverRequest -> ok().body(br.findAll(), Book.class));
     }
 
     @RequestMapping(value = "/books")
-      public String index(Model model) {
-    	model.addAttribute("message", "data");
+      public String index(final Model model) {
+    
+        IReactiveDataDriverContextVariable books =
+                new ReactiveDataDriverContextVariable(bookRepository.findAll());
+
+        model.addAttribute("books", books);
+    	
     	return "index";
       }
 }
--- src/main/java/com/booksearch/BookRepository.java
+++ src/main/java/com/booksearch/BookRepository.java
@@ -1,6 +1,11 @@
 package com.booksearch;
 
+import java.util.ArrayList;
+
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
+import reactor.core.publisher.Flux;
+
 public interface BookRepository extends ReactiveMongoRepository<Book, String> {
+	Flux<Book> findAll();
 }
