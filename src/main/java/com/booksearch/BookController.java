b71b9ac8bf78150e88b4cc4485fa1c7d3c776bfc
Ed Werner <android.devops@gmail.com>
2019-07-11 17:02:17 -0400

    adds search rest template and query string

--- src/main/java/com/booksearch/BookController.java
+++ src/main/java/com/booksearch/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -17,32 +18,40 @@ import reactor.core.publisher.Flux;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Controller
 public class BookController {
 
-    private final BookRepository bookRepository;
+	private final BookRepository bookRepository;
 
-    BookController(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
-    
-    @Bean
-    RouterFunction<ServerResponse> routes(BookRepository br) {
-        return RouterFunctions.route(GET("/bookfeed"), serverRequest -> ok().body(br.findAll(), Book.class));
-    }
+	BookController(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
 
-    @RequestMapping(value = "/books")
-      public String index(final Model model) {
-    
-//        IReactiveDataDriverContextVariable books =
-//                new ReactiveDataDriverContextVariable(bookRepository.findAll());
+	@Bean
+	RouterFunction<ServerResponse> routes(BookRepository br) {
+		return RouterFunctions.route(GET("/bookfeed"), serverRequest -> ok().body(br.findAll(), Book.class));
+	}
 
-        model.addAttribute("books", bookRepository.findAll());
-    	
-    	return "index";
-      }
+	@RequestMapping(value = "/books")
+	public String books(final Model model) {
+
+		// IReactiveDataDriverContextVariable books =
+		// new ReactiveDataDriverContextVariable(bookRepository.findAll());
+
+		model.addAttribute("books", bookRepository.findAll());
+
+		return "index";
+	}
+
+	@RequestMapping(value = "/search")
+	public String search(final Model model, @RequestParam("term") String term) {
+		
+		System.out.println("TERM: " + term);
+		
+		
+		return "search";
+	}
 }
