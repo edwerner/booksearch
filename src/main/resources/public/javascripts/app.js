5dd8ec9d2bd4a2f14d1c0cd0f9a4defcaca2ab7e
Ed Werner <android.devops@gmail.com>
2019-10-31 21:14:19 -0400

    adds pagination button active state

--- src/main/resources/public/javascripts/app.js
+++ src/main/resources/public/javascripts/app.js
@@ -4,7 +4,6 @@
 		book.onclick = function() {
 			alert("hello world");
 		}
-		$("#results-term").text(getUrlParameter("term"));
 	});
 	
 	$(window).on('load', function() {
@@ -13,12 +12,4 @@
 		  top: "34%"
 		}, 2000).removeClass("fade");
 	});
-	
-	function getUrlParameter(name) {
-    	name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    	var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-	    var results = regex.exec(location.search);
-	    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-	};
-	
 })();
